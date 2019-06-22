c5bef3fbe75b65dacb226755beab842784e167d3
pythonprosys <51963753+pythonprosys@users.noreply.github.com>
2019-06-22 16:37:44 +0200

    Discord-Bot example

---
+++ discordbot-example.py
@@ -0,0 +1,18 @@
+#Python3
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run('XXXXXXXXXXXXXXXXXXXXXX') #Paste your Token here
